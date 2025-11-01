abd7f98450e5c7c2117702c51117b4903d14bfa9
tonilingardsson <toni.lingardsson@gmail.com>
2025-11-01 09:49:18 +0100

    Updated startEngine method with virtual to customize sounds.

--- Vehicles/Vehicle.cs
+++ Vehicles/Vehicle.cs
@@ -3,26 +3,26 @@ public class Vehicle
     // Physical props
     private int ManyWheels;
     private int WeightKg;
-    private string Color;
     // Identification props
     private string  Brand;
     private string Model;
     private int FabYear;
+    private string Color;
     // State props
     private bool IsWorking = true;
     private bool IsService = false;
 
-public Vehicle(int manyWheels, int weightKg, string color, string brand, string model, int fabYear)
+public Vehicle(int manyWheels, int weightKg, string brand, string model, int fabYear, string color)
 {
     ManyWheels = manyWheels;
     WeightKg = weightKg;
-    Color = color;
     Brand = brand;
     Model = model;
     FabYear = fabYear;
+    Color = color;
 
     // Method1 required in US7
-    public string startEngine()
+    public virtual string startEngine()
     {
         System.Console.WriteLine($"The XXXX is doing xxxx");
     }
